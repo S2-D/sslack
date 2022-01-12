5422ac015f421179e1ff71e2b23c203e8b0bbb28
Sodam Shin <damsoshin@gmail.com>
2022-01-13 01:05:37 +0900

    폴더 구조와 리액트 라우터

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Switch, Route, Redirect} from "react-router-dom"
+import LogIn from "@pages/LogIn"
+import SignUp from "@pages/SignUp"
 
 const App = () => {
-  return <div>안녕</div>;
+  return <Switch>
+    <Redirect exact path="/" to ="/login" />
+    {/*주소가 /면 /login 으로 */}
+    <Route path ="/login" component={LogIn}/>
+    <Route path ="/signup" component={SignUp}/>
+  </Switch>;
 };
 
 export default App;
