3e9efd85766bb7c9909f4b1dd40222dd8402d631
Sodam Shin <damsoshin@gmail.com>
2022-01-15 15:07:21 +0900

    회원가입 페이지 만들기

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,7 +1,102 @@
-import React from 'react';
+// import useInput from '@hooks/useInput';
+import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
+// import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { useCallback, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+// import useSWR from 'swr';
 
-const SignUp = ()=>{
-    return<div>회원가입</div>
-}
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
 
-export default SignUp
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(email, nickname, password, passwordCheck);
+      if (!setMismatchError) {
+        console.log('성공성공^^^');
+      }
+    },
+    [email, nickname, password, passwordCheck],
+    //deps에 state를 꼭 넣어줘야함 그래야 값이 업데이트.
+    // useCallback : 내부 콜백함수를 캐싱. 기억하고 deps의 state가 바뀌면 재실행.
+    // 안감싸면 리랜더링이 많이 일어남. (성능문제 X, 디버깅이 어려움)
+  );
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {/*{signUpError && <Error>이미 가입된 이메일입니다.</Error>}*/}
+          {/*{signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}*/}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <a href="/login">로그인 하러가기</a>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default SignUp;
