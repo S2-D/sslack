839786514b17de5b73fedf9e9965fb4bb177b26b
Sodam Shin <damsoshin@gmail.com>
2022-01-18 23:57:53 +0900

    커스텀 훅 만들기

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -4,22 +4,15 @@ import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } fro
 import axios from 'axios';
 import React, { useCallback, useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import useInput from '@hooks/useInput';
 // import useSWR from 'swr';
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
   const [mismatchError, setMismatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, []);
 
   const onChangePassword = useCallback(
     (e) => {
