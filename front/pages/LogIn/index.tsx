82df094fa9fd54c29e1786c78f68efb439a66a69
Sodam Shin <damsoshin@gmail.com>
2022-01-23 23:43:07 +0900

    수정

--- front/pages/LogIn/index.tsx
+++ front/pages/LogIn/index.tsx
@@ -29,12 +29,6 @@ const LogIn = () => {
     [email, password],
   );
 
-  // console.log(error, userData);
-  // if (!error && userData) {
-  //   console.log('로그인됨', userData);
-  //   return <Redirect to="/workspace/sleact/channel/일반" />;
-  // }
-
   return (
     <div id="container">
       <Header>Sleact</Header>
